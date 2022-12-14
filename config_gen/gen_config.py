9a92ae7d38f6aa2974750707080da61260bf3513
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2022-12-14 20:16:48 +0100

    Added scripts to generate configs

---
+++ config_gen/gen_config.py
@@ -0,0 +1,63 @@
+import json
+
+
+# change setttings here
+
+filename = 'normal_abnormal'
+# dataset filename
+ds_files = 'normal_abnormal.h5'
+
+# how did you resize the images
+input_size = 640
+# lower learning rates for pretrain models
+learning_rates = [0.0001, 0.001]
+# which EfficientNet
+model_types = ['B0', 'B1', 'B2', 'B3']
+# how many classes, pretrain or from scratch
+num_class = 2
+pretrain = True
+
+
+base_ds_path = '/mnt/project/ngoc/CubiAI/datasets/'
+if pretrain:
+    if num_class <= 2:
+        template_fn = 'config/local/pretrain.json'
+    else:
+        template_fn = 'config/local/pretrain_multiclass.json'
+else:
+    if num_class <= 2:
+        template_fn = 'config/local/scratch.json'
+    else:
+        template_fn = 'config/local/scratch_multiclass.json'
+
+train_type = 'pretrain' if pretrain else 'scratch'
+
+with open(template_fn, 'r') as f:
+    template = json.load(f)
+
+# update batch size and cache
+template['dataset_params']['config']['batch_size'] = 16
+template['dataset_params']['config']['batch_cache'] = 8
+
+for lr in learning_rates:
+    lr_str = f'{lr:6.5f}'.split('.')[-1]
+    for eff_type in model_types:
+        # update input size
+        template['input_params']['shape'][0] = input_size
+        template['input_params']['shape'][1] = input_size
+
+        # update preprocessors
+        template['dataset_params']['config']['preprocessors'][0]['config']['num_class'] = num_class
+
+        # update model architecture
+        template['architecture']['num_class'] = num_class
+        template['architecture']['pretrained'] = pretrain
+        template['architecture']['class_name'] = eff_type
+
+        # update learning rate
+        template['model_params']['optimizer']['config']['learning_rate'] = lr
+
+        model = eff_type.lower()
+
+        with open(f'config/{train_type}/{model}_{filename}_lr_{lr_str}.json', 'w') as f:
+            json.dump(template, f)
