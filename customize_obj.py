886bf81cf5c8dd19a275cf1d73d4818e33737711
sunnivasteiro <sunnivasteiro@live.no>
2023-02-15 11:04:55 +0100

    fixed problem of flipped targets for binary exp.s

--- customize_obj.py
+++ customize_obj.py
@@ -87,5 +87,7 @@ class OneHot(BasePreprocessor):
         new_targets = np.zeros((len(targets), self.num_class))
         for i in range(self.num_class):
             new_targets[..., i][targets == i] = 1
+        if self.num_class==1:
+            new_targets[..., 0] = targets
 
         return images, new_targets
