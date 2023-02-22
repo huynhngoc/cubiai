5099a6422ddd312485a6c99c54ed083a3a22f3a8
sunnivasteiro <sunnivasteiro@live.no>
2023-02-22 11:46:24 +0100

    new config

--- config_gen/gen_config copy.py
+++ config_gen/gen_config copy.py
@@ -3,18 +3,18 @@ import json
 
 # change setttings here
 
-filename = '800_normal_abnormal_1_bs25'
+filename = '1280_normal_abnormal_2_bs16'
 # dataset filename
-ds_files = '800_normal_abnormal_1.h5'
+ds_files = '1280_normal_abnormal_2.h5'
 
 #batch size
-bs = 25
+bs = 16
 # how did you resize the images
-input_size = 800
+input_size = 1280
 # lower learning rates for pretrain models
 learning_rates = [0.0005]
 # which EfficientNet
-model_types = ['B1']
+model_types = ['B3']
 # how many classes, pretrain or from scratch
 num_class = 2
 pretrain = True
