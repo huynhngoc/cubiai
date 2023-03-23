6eee3d3741220e7087355eebc05fc23010cb563d
sunnivasteiro <sunnivasteiro@live.no>
2023-03-23 16:58:39 +0100

    make output filename external instead of pretrain

--- slurm_external_binary.sh
+++ slurm_external_binary.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --ntasks=1               # 1 core(CPU)
 #SBATCH --nodes=1                # Use 1 node
-#SBATCH --job-name=CubiAI_pretrain   # sensible name for the job
+#SBATCH --job-name=CubiAI_external   # sensible name for the job
 #SBATCH --mem=32G                 # Default memory per CPU is 3GB.
 #SBATCH --partition=gpu # Use the verysmallmem-partition for jobs requiring < 10 GB RAM.
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-user=sunniva.elisabeth.daae.steiro@nmbu.no # Email me when job is done.
 #SBATCH --mail-type=ALL
-#SBATCH --output=outputs/pretrain-%A.out
-#SBATCH --error=outputs/pretrain-%A.out
+#SBATCH --output=outputs/external-%A.out
+#SBATCH --error=outputs/external-%A.out
 
 # If you would like to use more please adjust this.
 
