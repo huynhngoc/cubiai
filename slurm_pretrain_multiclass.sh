56ef4ac30cd2028bbea37803978ca30c07269470
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2024-01-17 16:14:06 +0100

    Added config b4 kappa loss & update slurm script

--- slurm_pretrain_multiclass.sh
+++ slurm_pretrain_multiclass.sh
@@ -2,11 +2,11 @@
 #SBATCH --ntasks=1               # 1 core(CPU)
 #SBATCH --nodes=1                # Use 1 node
 #SBATCH --job-name=CubiAI_pretrain   # sensible name for the job
-#SBATCH --mem=32G                 # Default memory per CPU is 3GB.
+#SBATCH --mem=64G                 # Default memory per CPU is 3GB.
 #SBATCH --partition=gpu # Use the verysmallmem-partition for jobs requiring < 10 GB RAM.
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=16
-#SBATCH --mail-user=sunniva.elisabeth.daae.steiro@nmbu.no # Email me when job is done.
+#SBATCH --mail-user=$USER@nmbu.no # Email me when job is done.
 #SBATCH --mail-type=ALL
 #SBATCH --output=outputs/pretrain-%A.out
 #SBATCH --error=outputs/pretrain-%A.out
@@ -26,22 +26,22 @@ if [ $# -lt 3 ];
     exit 0
     fi
 
-if [ ! -d "$TMPDIR/$USER/CubiAI" ]
-    then
-    echo "Didn't find dataset folder. Copying files..."
-    mkdir --parents $TMPDIR/$USER/CubiAI
-    fi
+# if [ ! -d "$TMPDIR/$USER/CubiAI" ]
+#     then
+#     echo "Didn't find dataset folder. Copying files..."
+#     mkdir --parents $TMPDIR/$USER/CubiAI
+#     fi
 
-for f in $(ls $PROJECTS/ngoc/CubiAI/datasets/*)
-    do
-    FILENAME=`echo $f | awk -F/ '{print $NF}'`
-    echo $FILENAME
-    if [ ! -f "$TMPDIR/$USER/CubiAI/$FILENAME" ]
-        then
-        echo "copying $f"
-        cp -r $PROJECTS/ngoc/CubiAI/datasets/$FILENAME $TMPDIR/$USER/CubiAI/
-        fi
-    done
+# for f in $(ls $PROJECTS/ngoc/CubiAI/datasets/*)
+#     do
+#     FILENAME=`echo $f | awk -F/ '{print $NF}'`
+#     echo $FILENAME
+#     if [ ! -f "$TMPDIR/$USER/CubiAI/$FILENAME" ]
+#         then
+#         echo "copying $f"
+#         cp -r $PROJECTS/ngoc/CubiAI/datasets/$FILENAME $TMPDIR/$USER/CubiAI/
+#         fi
+#     done
 
 
 echo "Finished seting up files."
@@ -50,7 +50,7 @@ echo "Finished seting up files."
 nvidia-modprobe -u -c=0
 
 # Run experiment
-# export ITER_PER_EPOCH=200
+export ITER_PER_EPOCH=128
 export NUM_CPUS=4
 export RAY_ROOT=$TMPDIR/$USER/ray
 singularity exec --nv deoxys.sif python experiment_multiclass.py $1 $PROJECTS/ngoc/CubiAI/perf/pretrain/$2 --temp_folder $SCRATCH_PROJECTS/ceheads/CubiAI/pretrain/$2 --epochs $3 ${@:4}
